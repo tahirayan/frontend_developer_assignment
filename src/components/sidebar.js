79cf45710690a642dee29d036fe8892732c610a8
tahirayan <tahirayan15@gmail.com>
2021-03-23 19:52:04 +0300

    About links activated

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -24,10 +24,10 @@ export default function Sidebar() {
                     <button className={styles.accordion}>Linktera Hakkında</button>
                     </div>
                     <div className={styles.panel}>
-                        <img src={right} alt={""} /><a href={"https://www.linktera.com.tr/"} className={styles.links}>Linktera Websitesi</a>
-                        <img src={right} alt={""} /><a href={"https://www.linkedin.com/company/linktera/"} className={styles.links}>LinkedIn Hesabı</a>
-                        <img src={right} alt={""} /><a href={"https://www.youtube.com/channel/UCLso6-PsSNgnSU1ZW2Fva7Q"} className={styles.links}>Youtube Kanalı</a>
-                        <img src={right} alt={""} /><a href={"https://www.linktera.com.tr/"} className={styles.links}>Oryantasyon Sunumu</a>
+                        <div><img src={right} alt={""} /><a href={"https://www.linktera.com.tr/"} className={styles.links}>Linktera Websitesi</a></div>
+                        <div><img src={right} alt={""} /><a href={"https://www.linkedin.com/company/linktera/"} className={styles.links}>LinkedIn Hesabı</a></div>
+                        <div><img src={right} alt={""} /><a href={"https://www.youtube.com/channel/UCLso6-PsSNgnSU1ZW2Fva7Q"} className={styles.links}>Youtube Kanalı</a></div>
+                        <div><img src={right} alt={""} /><a href={"https://www.linktera.com.tr/"} className={styles.links}>Oryantasyon Sunumu</a></div>
                     </div>
                     <div className={styles.wrapper}>
                         <img src={arrow} alt={""} />
@@ -55,7 +55,8 @@ export default function Sidebar() {
                     </div>
                 </nav>
                 <div className={styles.logout}>
-                    <p>Çıkış</p><svg xmlns="http://www.w3.org/2000/svg" width="23.238" height="33.977" viewBox="0 0 23.238 33.977">
+                    <p>Çıkış</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="23.238" height="33.977" viewBox="0 0 23.238 33.977">
                     <path id="Path_6" data-name="Path 6" d="M161.949,487.4v27.618l16,6.359V492.443H165.019v-1.754h15.125v24.33h5.042V487.4Zm5.563,14.509a.79.79,0,0,1,.791-.789h2.132a.794.794,0,0,0,.791-.791.79.79,0,0,1,1.345-.563l3.353,3.326-3.353,3.324a.777.777,0,0,1-.552.23.788.788,0,0,1-.793-.793.79.79,0,0,0-.791-.789H168.3a.79.79,0,0,1-.791-.789Z" transform="translate(-161.949 -487.401)" fill="#fff"/>
                 </svg>
                 </div>
